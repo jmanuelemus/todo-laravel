24893e858f39ea35e4addb6da99bf24a3ab8e20b
Juan Manuel Lemus <jmanuelemus@gmail.com>
2020-10-17 19:21:45 -0600

    Add Schema to Laravel compatible timestamp accessors

--- app/Schema.php
+++ app/Schema.php
@@ -29,11 +29,83 @@ class Schema extends Model
     const UPDATED_AT = '_updated_at';
 
     /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        //
+    ];
+
+    /**
      * The primary key for the model
      * 
      * @var string
      */
     protected $primaryKey = '_id';
+
+    /**
+     * Get 'created_at' attribute from the model.
+     * 
+     * @return \Carbon\Carbon
+     */
+    public function getCreatedAtAttribute()
+    {
+        return $this->attributes[self::CREATED_AT];
+    }
+
+    /**
+     * Set 'created_at' attribute on the model.
+     * 
+     * @param  \Carbon\Carbon|string  $value
+     * @return void
+     */    
+    public function setCreatedAtAttribute($value)
+    {
+        $this->attributes[self::CREATED_AT] = $value;
+    }
+
+    /**
+     * Get 'deleted_at' attribute from the model.
+     * 
+     * @return \Carbon\Carbon
+     */
+    public function getDeletedAtAttribute()
+    {
+        return $this->attributes[self::DELETED_AT];
+    }
+
+    /**
+     * Set 'deleted_at' attribute on the model.
+     * 
+     * @param  \Carbon\Carbon|string  $value
+     * @return void
+     */    
+    public function setDeletedAtAttribute($value)
+    {
+        $this->attributes[self::DELETED_AT] = $value;
+    }
+
+    /**
+     * Get 'updated_at' attribute from the model.
+     * 
+     * @return \Carbon\Carbon
+     */
+    public function getUpdatedAtAttribute()
+    {
+        return $this->attributes[self::UPDATED_AT];
+    }
+
+    /**
+     * Set 'updated_at' attribute on the model.
+     * 
+     * @param  \Carbon\Carbon|string  $value
+     * @return void
+     */    
+    public function setUpdatedAtAttribute($value)
+    {
+        $this->attributes[self::UPDATED_AT] = $value;
+    }
 
     /**
      * Get 'id' attribute from the model.
