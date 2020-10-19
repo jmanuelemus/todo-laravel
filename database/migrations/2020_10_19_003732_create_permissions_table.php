98129ef695b3f86988ad337ec71ad7fa177ce7b5
Juan Manuel Lemus <jmanuelemus@gmail.com>
2020-10-18 18:53:41 -0600

    Add index to Permission schema

--- database/migrations/2020_10_19_003732_create_permissions_table.php
+++ database/migrations/2020_10_19_003732_create_permissions_table.php
@@ -15,7 +15,7 @@ class CreatePermissionsTable extends Migration
     {
         Schema::create('permissions', function (Blueprint $table) {
             $table->bigIncrements('_id');
-            $table->string('name', 32);
+            $table->string('name', 32)->index();
             $table->timestamp('_created_at');
             $table->timestamp('_updated_at')->nullable();
             $table->timestamp('_deleted_at')->nullable();
